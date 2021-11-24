3ef824e812a556da6d4a859f4943462560d78e61
yujia <yujia.yjq@alibaba-inc.com>
2021-11-24 14:50:28 +0800

    first commit

---
+++ src/magix5-gallery/preloadModuleList.ts
@@ -0,0 +1,3 @@
+export default [
+  'magix5-gallery/prepare','magix5-gallery/view','magix5-gallery/web_modules/blueimp-md5','magix5-gallery/views/default','magix5-gallery/services/jsvm','magix5-gallery/services/models','magix5-gallery/services/service','magix5-gallery/gallery/mx-test/index','magix5-gallery/gallery/mx-form/refs','magix5-gallery/gallery/mx-form/sync','magix5-gallery/dataplus/config','magix5-gallery/chartpark/index'
+]
--- src/magix5-gallery/views/default.ts
+++ src/magix5-gallery/views/default.ts
@@ -8,20 +8,24 @@ export default ProjectView.extend({
   tmpl: '@:./default.html',
   async render() {
     let renderMark = mark(this, '@:{render.mark}');
-    let [bag] = await this.fetch({
-      name: 'api_tag_topic_list_get'
-    });
-    let list = bag.get('data.list', []);
+    // let [bag] = await this.fetch({
+    //   name: 'api_tag_topic_list_get'
+    // });
+    // let list = bag.get('data.list', []);
     if (renderMark()) {
       await this.digest({
         user: {
           name: 'magix5-gallery'
         },
-        list
+        list: [{
+          month: 1,
+        }, {
+          month: 2,
+        }]
       })
     }
   },
   'log<click>'() {
-    console.log(this.get(),this.$refs);
+    console.log(this.get(), this.$refs);
   }
 });
