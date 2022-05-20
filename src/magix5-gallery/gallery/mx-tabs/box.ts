b9242357584322f7d8b35ee6e552b46a103838d7
yanhong <yanhong.xhj@alibaba-inc.com>
2022-05-20 13:56:46 +0800

    auto commit by mm-cli

--- src/magix5-gallery/gallery/mx-tabs/box.ts
+++ src/magix5-gallery/gallery/mx-tabs/box.ts
@@ -5,106 +5,115 @@ Magix5.applyStyle('@:box.less');
 // TODO:mode 支持shadow、vertical；
 
 interface Item {
-  text: string
-  value: string | number
-  tag?: string
-  color?: string
-  tip?: string
-  tips?:string // 兼容
-  img?: string
-  disabled?: boolean
+    text: string;
+    value: string | number;
+    tag?: string;
+    color?: string;
+    tip?: string;
+    tips?: string; // 兼容
+    img?: string;
+    disabled?: boolean;
 }
 
-enum Size{
-  SMALL = 'small',
-  NORMAL = 'normal', // 默认
-  LARGE ='large'
+enum Size {
+    SMALL = 'small',
+    NORMAL = 'normal', // 默认
+    LARGE = 'large',
 }
 
-enum Mode{
-  SHADOW = 'shadow', // 阴影
-  SPLITER = 'spliter', // 分割线,默认
-  VERTICAL = 'vertical' // 竖版
+enum Mode {
+    SHADOW = 'shadow', // 阴影
+    SPLITER = 'spliter', // 分割线,默认
+    VERTICAL = 'vertical', // 竖版
 }
 
-interface Options{
-  list: Item[]
-  selected?:any
-  disabled?: boolean
-  textKey?:string // default:text
-  valueKey?:string // default:value
-  mode?:Mode
-  size?:Size
+interface Options {
+    list: Item[];
+    selected?: any;
+    disabled?: boolean;
+    textKey?: string; // default:text
+    valueKey?: string; // default:value
+    mode?: Mode;
+    size?: Size;
 }
 
 export default View.extend({
-  tmpl:'@:box.html',
-  assign(options:Options) {
-    const data = this.initStateFromOpts(options)
-    this.set({
-      ...data,
-      originOpts:options
-    })
-  },
+    tmpl: '@:box.html',
+    assign(options: Options) {
+        const data = this.initStateFromOpts(options);
+        this.set({
+            ...data,
+            originOpts: options,
+            classesText(item, selected) {
+                const { disabled, value } = item;
+                const classes = [];
+                value == selected && classes.push('active-item');
+                classes.push(disabled == true ? 'disabled' : 'enabled');
+                return classes.join(' ');
+            },
+        });
+    },
 
-  async render() {
-    await this.digest()
-  },
+    async render() {
+        await this.digest();
+    },
 
-  initStateFromOpts(options:Options) {
-    const disabled = options.disabled + '' === 'true' // disabled
-    const textKey = options.textKey || 'text'
-    const valueKey = options.valueKey || 'value'
-    
-    const originList = options.list || [] 
-    const list = originList.map(item => {
-      return {
-        ...item,
-        text:item[textKey],
-        value:item[valueKey],
-        color: item.color||'',
-        tip: item.tip || item.tips || '',
-        disabled:disabled || ( item.disabled + '' === 'true' )
-      }
-    })
+    initStateFromOpts(options: Options) {
+        const disabled = options.disabled + '' === 'true'; // disabled
+        const textKey = options.textKey || 'text';
+        const valueKey = options.valueKey || 'value';
 
-    const selected = options.selected || ( list[0]|| {} )['value'] // default:first one
-    const size = ['normal','small','large'].includes(options.size) ? options.size :Size.NORMAL
-    const mode = ['shadow', 'spliter', 'vertical'].includes(options.mode) ? options.mode: Mode.SPLITER
+        const originList = options.list || [];
+        const list = originList.map((item) => {
+            return {
+                ...item,
+                text: item[textKey],
+                value: item[valueKey],
+                color: item.color || '',
+                tip: item.tip || item.tips || '',
+                disabled: disabled || item.disabled + '' === 'true',
+            };
+        });
 
-    return {
-      list,
-      selected,
-      disabled,
-      size,
-      mode
-    }
-  },
+        const selected = options.selected || (list[0] || {})['value']; // default:first one
+        const size = ['normal', 'small', 'large'].includes(options.size)
+            ? options.size
+            : Size.NORMAL;
+        const mode = ['shadow', 'spliter', 'vertical'].includes(options.mode)
+            ? options.mode
+            : Mode.SPLITER;
 
-  /* event handler */
-  '@:{select}<click>'(e) {
-    const {value} = e.params
-    this['@:{fire}'](value)
-  },
+        return {
+            list,
+            selected,
+            disabled,
+            size,
+            mode,
+        };
+    },
 
-  '@:{fire}'(value) {
-    const {selected:oldSelected,list} = this.get()
-    const selectedItem = list.filter(d => d.value == value)?.[0]
-    if(oldSelected == value || !selectedItem) {
-      return
-    }
+    /* event handler */
+    '@:{select}<click>'(e) {
+        const { value } = e.params;
+        this['@:{fire}'](value);
+    },
 
-    // digest
-    this.digest({
-      selected:value
-    })
+    '@:{fire}'(value) {
+        const { selected: oldSelected, list } = this.get();
+        const selectedItem = list.filter((d) => d.value == value)?.[0];
+        if (oldSelected == value || !selectedItem) {
+            return;
+        }
 
-    // dispatch
-    Magix5.dispatch(this.root,
-      'change',
-      {
-        selected:selectedItem.value,
-        text:selectedItem.text
-    })
-  }
-})
+        // digest
+        this.digest({
+            selected: value,
+        });
+
+        // dispatch
+        Magix5.dispatch(this.root, 'change', {
+            selected: selectedItem.value,
+            text: selectedItem.text,
+        });
+    },
+});
