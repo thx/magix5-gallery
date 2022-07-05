1a161a6bb7964e888f7744a890b6374b7f27b7fa
yujia <yujia.yjq@alibaba-inc.com>
2022-07-05 18:56:50 +0800

    minisite tab-content  detail

--- src/magix5-gallery/views/pages/minisite/detail.ts
+++ src/magix5-gallery/views/pages/minisite/detail.ts
@@ -1,10 +1,10 @@
 import Magix5, {applyStyle} from 'magix5';
 import View from './base';
-applyStyle('@index.less');
-applyStyle('@detail.less');
+applyStyle('@:index.less');
+applyStyle('@:detail.less');
 
 export default View.extend({
-    tmpl: '@detail.html'
+    tmpl: '@:detail.html'
 });
 
 
